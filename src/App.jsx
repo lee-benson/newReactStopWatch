c38b7b8bb15baaf84a06fd1ad37b5d7025465a2a
Benson Lee <BensonL1334@gmail.com>
2023-03-07 13:27:57 -0500

    hello

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Stopwatch from "./components/Stopwatch";
+import './App.css';
+
+function App() {
+  return (
+    <Stopwatch />
+  );
+}
+
+
+
+export default App;
